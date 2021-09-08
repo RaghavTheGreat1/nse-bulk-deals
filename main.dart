7e83c385f4d53b15507a7ca35d48c9dacc9c9362
Raghav Joshi <developer.raghavjoshi@gmail.com>
2021-09-08 22:14:00 +0530

    print all bulk deals

--- main.dart
+++ main.dart
@@ -1,8 +1,22 @@
+import 'dart:io';
+
 // TODO: Number of scrips traded.
 // TODO: List of scrips traded.
 // TODO: Net shares traded in bulk deals overall.
 // TODO: Net shares traded in bulk deals scripwise
 
-void main() {
-  print("Hello Bulls!");
+void main() async {
+// Creates a file object.
+  var bulkDealsFile = File("bulk.csv");
+  List<String> content = [];
+
+// Fetches & assign [List] of each lines to content.
+  await bulkDealsFile.readAsLines().then((List<String> value) {
+    content = value;
+  });
+
+// Displays all bulk deals.
+  for (int i = 0; i < content.length; i++) {
+    print("${content[i]}");
+  }
 }
