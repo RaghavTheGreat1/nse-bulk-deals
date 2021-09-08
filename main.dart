a37aad7bdc03801f57eb63513a3aa4a3a51e1ddb
Raghav Joshi <developer.raghavjoshi@gmail.com>
2021-09-08 21:38:26 +0530

    Added TODOs

---
+++ main.dart
@@ -0,0 +1,8 @@
+// TODO: Number of scrips traded.
+// TODO: List of scrips traded.
+// TODO: Net shares traded in bulk deals overall.
+// TODO: Net shares traded in bulk deals scripwise
+
+void main() {
+  print("Hello Bulls!");
+}
